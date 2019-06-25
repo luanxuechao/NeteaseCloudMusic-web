0e4a5a8413dde77d4a97e1ccbea3cc036750ce94
luanwin <luanxuechaowd@gmail.com>
2019-06-25 21:12:07 +0800

    feat:daily commit

--- components/header/HeaderBar.vue
+++ components/header/HeaderBar.vue
@@ -1,31 +1,31 @@
 <template>
 <div class="g-topbar">
     <div class="m-top">
-        <div class="wrap f-cb">
-            <h1 class="logo">
+        <div class="wrap ">
+            <h1 class="logo f-cb">
                 <a hidefocus="true" href="/#">网易云音乐</a>
             </h1>
+            <ul class="m-nav">
+                <li class="fst">
+                    <span><a hidefocus="true" href="/#"  class="z-slt"><em>发现音乐</em><sub class="cor">&nbsp;</sub></a></span>
+                </li>
+                <li>
+                    <span><a data-action="bilog"  ><em>我的音乐</em><sub class="cor">&nbsp;</sub></a></span>
+                </li>
+                <li>
+                    <span><a hidefocus="true"><em>朋友</em><sub class="cor">&nbsp;</sub><i class="dot j-t" style="display:none;"></i></a></span>
+                </li>
+                <li>
+                    <span><a hidefocus="true"  target="_blank"><em>商城</em></a></span>
+                </li>
+                <li>
+                    <span><a hidefocus="true"  target="_blank" ><em>音乐人</em></a></span>
+                </li>
+                <li class="lst">
+                    <span><a ><em>下载客户端</em><sub class="cor">&nbsp;</sub></a></span><sup class="hot">1111</sup>
+                </li>
+            </ul>
         </div>
-        <ul class="m-nav j-tflag">
-            <li class="fst" >
-                <span><a hidefocus="true" href="/#" data-module="discover" class="z-slt"><em>发现音乐</em><sub class="cor">&nbsp;</sub></a></span>
-            </li>
-            <li>
-                <span><a data-action="bilog" data-log-action="page" data-log-json="{&quot;type&quot;:&quot;my&quot;}" hidefocus="true" href="/my/" data-module="my"><em>我的音乐</em><sub class="cor">&nbsp;</sub></a></span>
-            </li>
-            <li>
-                <span><a hidefocus="true" href="/friend" data-module="friend"><em>朋友</em><sub class="cor">&nbsp;</sub><i class="dot j-t" style="display:none;"></i></a></span>
-            </li>
-            <li>
-                <span><a hidefocus="true" href="/store/product" target="_blank" data-module="store"><em>商城</em></a></span>
-            </li>
-            <li>
-                <span><a hidefocus="true" href="/nmusician/web/recruit" target="_blank" data-module="musician"><em>音乐人</em></a></span>
-            </li>
-            <li class="lst">
-                <span><a id="topbar-download-link" data-action="bilog" data-log-action="page" data-log-json="{&quot;type&quot;:&quot;downloadapp&quot;,&quot;source&quot;:&quot;tab&quot;}" hidefocus="true" href="/download" data-module="download"><em>下载客户端</em><sub class="cor">&nbsp;</sub></a></span><sup class="hot">&nbsp;</sup>
-            </li>
-        </ul>
     </div>
 </div>
 </template>
